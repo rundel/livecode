d2c7e2e2f99ee7f99728dccac413e0ebb707dc14
Colin Rundel <rundel@gmail.com>
2019-08-14 15:51:33 +0100

    bitly stuff

---
+++ R/bitly_api.R
@@ -0,0 +1,71 @@
+bitly_api_host = "https://api-ssl.bitly.com/v4"
+
+req_func = function(method) {
+  method = toupper(method)
+  switch(
+    method,
+    GET    = httr::GET,
+    POST   = httr::POST,
+    PATCH  = httr::PATCH,
+    PUT    = httr::PUT,
+    DELETE = httr::DELETE,
+    usethis::ui_stop("Unknown http method: {usethis::ui_value(method)}.")
+  )
+}
+
+bitly_api = function(endpoint, method = "GET", ..., token = bitly_get_token()) {
+  req = req_func(method)(
+    file.path(bitly_api_host, endpoint),
+    httr::add_headers(
+      Authorization = paste("Bearer", token)
+    ),
+    encode = "json",
+    body = list(...)
+  )
+
+  res = httr::content(req)
+
+  if (httr::status_code(req) != 200) {
+    usethis::ui_stop( "{res[['description']]} ({res[['message']]})" )
+  }
+
+  res
+}
+
+bitly_api_user = function() {
+  bitly_api("/user")
+}
+
+bitly_api_groups = function() {
+  bitly_api("/groups")[["groups"]]
+}
+
+bitly_api_shorten = function(long_url, group_guid = bitly_get_groups()[1]) {
+  bitly_api(
+    "/shorten", "POST",
+    group_guid = group_guid,
+    domain = "bit.ly",
+    long_url = URLencode(long_url)
+  )
+}
+
+#' @export
+bitly_get_groups = function() {
+  l = bitly_api_groups()
+  names = purrr::map_chr(l, "name")
+  guids = purrr::map_chr(l, "guid")
+
+  setNames(guids, names)
+}
+
+#' @export
+bitly_shorten = function(url, guid = bitly_get_groups()[1]) {
+  # TODO: add group handling
+  link = bitly_api_shorten(url, guid)[["link"]]
+
+  usethis::ui_done(
+    "Created a bit.ly shortlink {usethis::ui_value(link)} from {usethis::ui_value(url)}."
+  )
+
+  link
+}
---
+++ R/bitly_auth.R
@@ -0,0 +1,48 @@
+#' @export
+
+bitly_get_token = function() {
+  token = Sys.getenv("BITLY_PAT", "")
+  if (token != "")
+    return(invisible(token))
+
+  if (file.exists("~/.bitly/token")) {
+    bitly_set_token("~/.bitly/token")
+    return(invisible(bitly_get_token()))
+  }
+
+  usethis::ui_stop( paste0(
+    "Unable to locate bitly token, please use {usethis::ui_code('bitly_set_token')}",
+    " or define the BITLY_PAT environmental variable."
+  ) )
+}
+
+#' @export
+
+bitly_set_token = function(token) {
+  token = as.character(token)
+
+  if (file.exists(token))
+    token = readLines(token, warn=FALSE)
+
+  Sys.setenv(BITLY_PAT = token)
+}
+
+#' @export
+
+bitly_reset_token = function() {
+  Sys.unsetenv("BITLY_PAT")
+}
+
+
+#' @export
+
+bitly_test_token = function(token = bitly_get_token()) {
+  res = purrr::safely(gh::gh)("/user", .token=token)
+
+  status_msg(
+    res,
+    "Your bitly token is functioning correctly.",
+    "Your bitly token failed to authenticate.",
+  )
+}
+
