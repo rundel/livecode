185a3df714240606fa3423a3c4ff4e65f3d82633
Colin Rundel <cr173@duke.edu>
2020-02-23 19:13:20 -0500

    Initial attempt at windows network detection

--- src/get_ip.cpp
+++ src/get_ip.cpp
@@ -1,4 +1,70 @@
 #include <Rcpp.h>
+
+// [[Rcpp::plugins(cpp11)]]
+
+#if defined(WIN32) || defined(_WIN32)
+
+// From https://stackoverflow.com/questions/11588765/using-rcpp-with-windows-specific-includes
+#undef Realloc
+#undef Free
+
+#include <winsock2.h>
+#include <windows.h>
+#include <iphlpapi.h>
+#include <Ws2tcpip.h>
+
+#define WORKING_BUFFER_SIZE 15000
+
+std::string wide_to_str(wchar_t* pws) {
+  std::wstring ws(pws);
+
+  return std::string(ws.begin(), ws.end());
+}
+
+std::string sock_addr_to_str(SOCKET_ADDRESS addr) {
+  char buf[INET_ADDRSTRLEN];
+
+  memset(buf, 0, INET_ADDRSTRLEN);
+  getnameinfo(addr.lpSockaddr, addr.iSockaddrLength, buf, sizeof(buf), NULL, 0, NI_NUMERICHOST);
+
+  return std::string(buf);
+}
+
+
+Rcpp::CharacterVector get_ipv4_impl() {
+  Rcpp::CharacterVector ifaces;
+  Rcpp::CharacterVector addrs;
+
+  unsigned long int buf_len = 0;
+  GetAdaptersAddresses(AF_INET, 0, NULL, NULL, &buf_len);
+  Rcpp::Rcout << "buf_len: " << buf_len << "\n";
+  if (buf_len == 0)
+    Rcpp::stop("Retriving network adaptors failed!");
+
+  std::vector<char> mem(buf_len);
+  PIP_ADAPTER_ADDRESSES adapts = (PIP_ADAPTER_ADDRESSES) mem.data();
+
+  unsigned long int res = GetAdaptersAddresses(AF_INET, 0, NULL, adapts, &buf_len);
+  if (res != NO_ERROR)
+    Rcpp::stop("Retriving network adaptors failed!");
+
+  while (adapts != NULL) {
+    ifaces.push_back( wide_to_str(adapts->FriendlyName) );
+    addrs.push_back( sock_addr_to_str(adapts->FirstUnicastAddress->Address) );
+
+    //Rcpp::Rcout << "Name: " << wide_to_str(adapts->FriendlyName) << "\n";
+    //Rcpp::Rcout << "Desc: " << wide_to_str(adapts->Description) << "\n";
+    //Rcpp::Rcout << "Addr: " << sock_addr_to_str(adapts->FirstUnicastAddress->Address) << "\n";
+
+    adapts = adapts->Next;
+  };
+
+  addrs.attr("names") = ifaces;
+  return addrs;
+}
+
+#else
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <ifaddrs.h>
@@ -6,8 +72,7 @@
 #include <string.h>
 #include <arpa/inet.h>
 
-// [[Rcpp::export]]
-Rcpp::CharacterVector get_ipv4() {
+Rcpp::CharacterVector get_ipv4_impl() {
   Rcpp::CharacterVector ifaces;
   Rcpp::CharacterVector addrs;
 
@@ -37,6 +102,13 @@ Rcpp::CharacterVector get_ipv4() {
   return addrs;
 }
 
+#endif
+
+
+// [[Rcpp::export]]
+Rcpp::CharacterVector get_ipv4() {
+  return get_ipv4_impl();
+}
 /*** R
 get_ipv4()
 */
